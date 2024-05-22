fea7cca4fe19eccff542db3da017abbe3a262a2b
katekong <hqkate94@gmail.com>
2024-05-22 15:34:17 +0800

    refine test scripts

--- tests/test_vqvae_fp16.py
+++ tests/test_vqvae_fp16.py
@@ -8,6 +8,7 @@ sys.path.insert(0, os.path.abspath(os.path.join(__dir__, "..")))
 
 from videogvt.models.vqvae import VQVAE3D, StyleGANDiscriminator
 from videogvt.config.vqgan3d_magvit_v2_config import get_config
+from mindone.utils.amp import auto_mixed_precision
 
 # GRAPH_MODE - 0
 # PYNATIVE_MODE - 1
@@ -17,10 +18,19 @@ context.set_context(
 )
 
 config = get_config("B")
+bs = 1
+in_channels = 3
+crop_size = 256
+frame_size = 16
 model = VQVAE3D(config, lookup_free_quantization=True, is_training=True, dtype=ms.float16)
-discriminator = StyleGANDiscriminator(config, 128, 128, 16, dtype=ms.float16)
+discriminator = StyleGANDiscriminator(config, crop_size, crop_size, frame_size, dtype=ms.float16)
 
-x = ms.Tensor(np.random.rand(2, 3, 16, 128, 128), ms.float16)
+amp_level = "O2"
+dtype = ms.float16
+model = auto_mixed_precision(model, amp_level, dtype)
+discriminator = auto_mixed_precision(discriminator, amp_level, dtype)
+
+x = ms.Tensor(np.random.rand(bs, in_channels, frame_size, crop_size, crop_size), ms.float16)
 
 z_e, z_q, x_hat, aux_loss = model(x)
 
