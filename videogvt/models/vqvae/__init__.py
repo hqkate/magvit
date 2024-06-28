fe09dc9ad867dd24d4695d80ac3d5a892c0ce7ec
katekong <hqkate94@gmail.com>
2024-06-28 15:07:11 +0800

    add 3d-inflation

--- videogvt/models/vqvae/__init__.py
+++ videogvt/models/vqvae/__init__.py
@@ -1,2 +1,4 @@
 from .vqvae import VQVAE, VQVAE3D, VQVAEOpenSora
+from .vqvae_2d import VAE_KL_Spatial, VAE_KL_2D, VQVAE_Spatial, VQVAE_Magvit2_2D
+from .vqvae_3d import VAE_3D, VAE_KL_3D, VQVAE_Spatial, VQVAE_3D
 from .discriminator import StyleGANDiscriminator
